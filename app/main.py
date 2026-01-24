8270b074362d8f20933bd44b2efd891c84a7eb81
Muhammad <kayumovmuhamma@gmail.com>
2026-01-24 15:18:36 +0500

    add: interval trigger

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 from contextlib import asynccontextmanager
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from fastapi import FastAPI
 
 from app.parser.parser import write_timetable_to_json
 from app.routers import timetable_router
 
-scheduler = BackgroundScheduler()
+scheduler = AsyncIOScheduler()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    trigger = CronTrigger(hour=14, minute=52)
+    trigger = IntervalTrigger(hours=1, start_date=datetime.now())
+    write_timetable_to_json()
     scheduler.add_job(write_timetable_to_json, trigger)
     scheduler.start()
-    
-    write_timetable_to_json()
-    
+
     print("Start")
     yield
     print("End")
